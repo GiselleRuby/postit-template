599ac0b72bea19c90b7f9e737f3dfb49596af25e
blackwindsky <blackwindsky@gmail.com>
2014-08-25 23:56:28 +0800

    bugs fixed

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,5 +1,6 @@
 class Vote < ActiveRecord::Base
   belongs_to :creator, class_name: 'User', foreign_key: 'user_id'
   belongs_to :voteable, polymorphic: true
-  validates :creator, uniqueness: { scope: :voteable, message: "You can only vote once." }
+  # validates :creator, uniqueness: { scope: :voteable, message: "You can only vote once." }
+  validates_uniqueness_of :creator, scope: [:voteable_id, :voteable_type]
 end
